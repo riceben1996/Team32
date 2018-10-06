1cc76f1501284243cbb92873289b1e10cf5da34b
riceben1996 <riceben@indiana.edu>
2018-10-06 15:17:23 -0400

    Fixing comment

--- app/models/Comment.php
+++ app/models/Comment.php
@@ -6,18 +6,19 @@ class Comment
   public $comment;
 
   public function __construct($data) {
-    $this->id = isset($data['commentId']) ? $data['commentId'] : null;
-    $this->comment = $data['clientId'];
+    $this->commentId = isset($data['commentId']) ? $data['commentId'] : null;
+    $this->clientId = intval($data['clientId']);
     $this->comment = $data['comment'];
   }
 
   public function create() {
     $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    $sql = 'INSERT INTO comment (comment) VALUES (?)';
+    $sql = 'INSERT INTO comment (clientId, comment) VALUES (?, ?)';
 
     $statement = $db->prepare($sql);
 
     $success = $statement->execute([
+      $this->clientId,
       $this->comment,
     ]);
 
@@ -25,23 +26,25 @@ class Comment
       // TODO: Better error handling
       die('SQL error');
     }
-    $this->id = $db->lastInsertId();
+    $this->commentId = $db->lastInsertId();
   }
 
-  public static function fetchComments() {
-    // 1. Connect to the database
-    $db = new PDO(DB_SERVER, DB_USER, DB_PW);
-    // 2. Prepare the query
-    $sql = 'SELECT * FROM comment';
-    $statement = $db->prepare($sql);
-    // 3. Run the query
-    $success = $statement->execute();
-    // 4. Handle the results
-    $arr = [];
-    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-      $theComment =  new Comment($row);
-      array_push($arr, $theComment);
+  public static function fetchComments(int $clientId) {
+      // 1. Connect to the database
+      $db = new PDO(DB_SERVER, DB_USER, DB_PW);
+      // 2. Prepare the query
+      $sql = 'SELECT * FROM comment WHERE clientId =?';
+      $statement = $db->prepare($sql);
+      // 3. Run the query
+      $success = $statement->execute(
+        [$clientId]
+      );
+      // 4. Handle the results
+      $arr = [];
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $theComment =  new Comment($row);
+        array_push($arr, $theComment);
+      }
+      return $arr;
     }
-    return $arr;
-  }
 }
--- public/api/comment.php
+++ public/api/comment.php
@@ -7,8 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   exit;
 }
 
+$clientId = intval($_GET['clientId'] ?? 0);
+
 // 1. Go to the database and get all turbines
-$comments = Comment::fetchComments();
+$comments = Comment::fetchComments($clientId);
 // 2. Convert to JSON
 $json = json_encode($comments, JSON_PRETTY_PRINT);
 // 3. Print
