14d782e27a780ec117069bff15acce8bfc7b1d3f
riceben1996 <riceben@indiana.edu>
2018-10-27 17:49:22 -0400

    testing

--- public/api/sensorTimeSeries.php
+++ public/api/sensorTimeSeries.php
@@ -1,8 +1,11 @@
 <?php
 
 require '../../app/common.php';
-// 1. Go to the database and get all turbines
-$sensorTimeSeries = SensorTimeSeries::fetchSensorTimeSeries();
+
+$turbineDeployedId = intval($_GET['turbineDeployedId'] ?? 0);
+
+// 1. Go to the database and get all teams
+$sensorTimeSeries = SensorTimeSeries::fetchSensorTimeSeries($turbineDeployedId);
 // 2. Convert to JSON
 $json = json_encode($sensorTimeSeries, JSON_PRETTY_PRINT);
 // 3. Print
--- public/js/dashboard.app.js
+++ public/js/dashboard.app.js
@@ -1,9 +1,7 @@
 var dashboardApp = new Vue({
   el: '#dashboardApp',
   data: {
-
-    dashboardAppForm: [],
-
+        dashboard: [],
   },
 
   methods: {
@@ -19,22 +17,9 @@ var dashboardApp = new Vue({
   created() {
 
     const url = new URL(window.location.href);
-    console.log(url);
     const turbineDeployedId = url.searchParams.get("turbineDeployedId");
     console.log(turbineDeployedId);
     this.turbineDeployedId = turbineDeployedId;
-
-    fetch('api/turbineDeployed.php?siteId=' + turbineDeployedId)
-      .then(response => response.json())
-      .then(json => {
-        siteDetailsApp.siteDetails = json
-      })
-      .catch(err => {
-        console.log('ERROR:');
-        console.log(err);
-      })
-
-
 
   }
 })
