2ce7da14be98bc0d8e073e09cbd81b1c8c483b5f
riceben1996 <jodrmill@iu.edu>
2018-10-28 18:06:23 -0400

    adding gauge for starts chart

--- public/js/kpiStarts.app.js
+++ public/js/kpiStarts.app.js
@@ -1,101 +1,130 @@
 var kpiStartsApp = new Vue({
   el: '#kpiStarts',
   data: {
-    sensorTimeSeries: [],
+    kpiViewTS: [],
   },
 
-  methods:{
-    fetchSensorTimeSeries (turbineDeployedId){
-      fetch('/api/sensorTimeSeries.php?turbineDeployedId='+turbineDeployedId)
-      .then( response => response.json() )
-      .then( json => {
-        kpiStartsApp.sensorTimeSeries = json;
-        kpiStartsApp.formatData();
-        kpiStartsApp.buildStartsChart();
-      } )
-      .catch( err => {
-        console.log('Error getting data');
-        console.log(err);
-      })
-    },
-
-    formatData(){
-      this.sensorTimeSeries.forEach(
-        (entry, index, arr) => {
-          entry.dateCollected = Date.parse(entry.dataCollectedDate);
-          entry.starts = Number(entry.starts);
-        }
-      )
+  methods: {
+    fetchkpiViewTS(turbineDeployedId) {
+      fetch('/api/kpiViewTS.php?turbineDeployedId=' + turbineDeployedId)
+        .then(response => response.json())
+        .then(json => {
+          kpiStartsApp.kpiViewTS = json;
+          kpiStartsApp.buildStartsChart();
+        })
+        .catch(err => {
+          console.log('Error getting data Gauge');
+          console.log(err);
+        })
     },
 
     buildStartsChart() {
       Highcharts.chart('startsChart', {
+
           chart: {
-              zoomType: 'x'
+              type: 'gauge',
+              plotBackgroundColor: null,
+              plotBackgroundImage: null,
+              plotBorderWidth: 0,
+              plotShadow: false
           },
+
           title: {
-              text: 'Starts over Time'
+              text: 'Percentage Chance the Turbine Starts on a Day'
           },
-          xAxis: {
-              type: 'datetime',
-              title: {
-                text: 'Date'
-              }
-          },
-          yAxis: {
-              title: {
-                  text: 'Starts'
-              }
-          },
-          legend: {
-              enabled: false
-          },
-          plotOptions: {
-              area: {
-                  fillColor: {
-                      linearGradient: {
-                          x1: 0,
-                          y1: 0,
-                          x2: 0,
-                          y2: 1
-                      },
+
+          pane: {
+              startAngle: -150,
+              endAngle: 150,
+              background: [{
+                  backgroundColor: {
+                      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
                       stops: [
-                          [0, Highcharts.getOptions().colors[0]],
-                          [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                          [0, '#FFF'],
+                          [1, '#333']
                       ]
                   },
-                  marker: {
-                      radius: 2
+                  borderWidth: 0,
+                  outerRadius: '109%'
+              }, {
+                  backgroundColor: {
+                      linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1 },
+                      stops: [
+                          [0, '#333'],
+                          [1, '#FFF']
+                      ]
                   },
-                  lineWidth: 1,
-                  states: {
-                      hover: {
-                          lineWidth: 1
-                      }
-                  },
-                  threshold: null
-              }
+                  borderWidth: 1,
+                  outerRadius: '107%'
+              }, {
+                  // default background
+              }, {
+                  backgroundColor: '#DDD',
+                  borderWidth: 0,
+                  outerRadius: '105%',
+                  innerRadius: '103%'
+              }]
+          },
+
+          // the value axis
+          yAxis: {
+              min: 0,
+              max: 100,
+
+              minorTickInterval: 'auto',
+              minorTickWidth: 1,
+              minorTickLength: 10,
+              minorTickPosition: 'inside',
+              minorTickColor: '#666',
+
+              tickPixelInterval: 30,
+              tickWidth: 2,
+              tickPosition: 'inside',
+              tickLength: 10,
+              tickColor: '#666',
+              labels: {
+                  step: 2,
+                  rotation: 'auto'
+              },
+              title: {
+                  text: 'Start Rate Percentage'
+              },
+              plotBands: [{
+                  from: 75,
+                  to: 100,
+                  color: '#55BF3B' // green
+              }, {
+                  from: 60,
+                  to: 75,
+                  color: '#DDDF0D' // yellow
+              }, {
+                  from: 0,
+                  to: 75,
+                  color: '#DF5353' // red
+              }]
           },
 
           series: [{
-              type: 'area',
-              name: 'Starts/Date',
-              data: kpiStartsApp.sensorTimeSeries.map( entry=>
-                [entry.dateCollected, entry.starts]
-              )
+              name: 'Start Rate %',
+              data: [Number(kpiStartsApp.kpiViewTS[0].startsPercentage)],
+              tooltip: {
+                  valueSuffix: '%'
+              }
           }]
-      });
+      }
+    );
+
     },
   },
 
-  created () {
+  created() {
 
     const url = new URL(window.location.href);
     const turbineDeployedId = url.searchParams.get('turbineDeployedId');
-    console.log('Turbine: '+ turbineDeployedId);
+    console.log('Turbine: ' + turbineDeployedId);
     this.turbineDeployedId = turbineDeployedId;
 
-    this.fetchSensorTimeSeries(turbineDeployedId);
+    this.fetchkpiViewTS(turbineDeployedId);
 
   }
-})
+  })
