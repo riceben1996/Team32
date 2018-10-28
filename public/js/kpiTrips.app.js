4dd32525c9bc931ec6352d2fb776af55d5e27b08
riceben1996 <tsisodia@iu.edu>
2018-10-28 00:37:59 -0400

    added highchart

--- public/js/kpiTrips.app.js
+++ public/js/kpiTrips.app.js
@@ -4,22 +4,22 @@ var kpiTripsApp = new Vue({
     sensorTimeSeries: [],
   },
 
-  methods:{
-    fetchSensorTimeSeries (turbineDeployedId){
-      fetch('/api/sensorTimeSeries.php?turbineDeployedId='+turbineDeployedId)
-      .then( response => response.json() )
-      .then( json => {
-        kpiTripsApp.sensorTimeSeries = json;
-        kpiTripsApp.formatData();
-        kpiTripsApp.buildTripsChart();
-      } )
-      .catch( err => {
-        console.log('Error getting data');
-        console.log(err);
-      })
+  methods: {
+    fetchSensorTimeSeries(turbineDeployedId) {
+      fetch('/api/sensorTimeSeries.php?turbineDeployedId=' + turbineDeployedId)
+        .then(response => response.json())
+        .then(json => {
+          kpiTripsApp.sensorTimeSeries = json;
+          kpiTripsApp.formatData();
+          kpiTripsApp.buildTripsChart();
+        })
+        .catch(err => {
+          console.log('Error getting data');
+          console.log(err);
+        })
     },
 
-    formatData(){
+    formatData() {
       this.sensorTimeSeries.forEach(
         (entry, index, arr) => {
           entry.dateCollected = Date.parse(entry.dataCollectedDate);
@@ -28,13 +28,70 @@ var kpiTripsApp = new Vue({
       )
     },
 
+    buildTripsChart() {
+      Highcharts.chart('tripsChart', {
+        chart: {
+          zoomType: 'x'
+        },
+        title: {
+          text: 'Trips over Time'
+        },
+        xAxis: {
+          type: 'datetime',
+          title: {
+            text: 'Date'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Trips'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          area: {
+            fillColor: {
+              linearGradient: {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 1
+              },
+              stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+              ]
+            },
+            marker: {
+              radius: 2
+            },
+            lineWidth: 1,
+            states: {
+              hover: {
+                lineWidth: 1
+              }
+            },
+            threshold: null
+          }
+        },
 
+        series: [{
+          type: 'area',
+          name: 'Trips/Date',
+          data: kpiTripsApp.sensorTime.map(entry => [entry.dateCollected, entry.trips])
+        }]
+      });
 
-  created () {
+    },
+  },
+
+  created() {
 
     const url = new URL(window.location.href);
     const turbineDeployedId = url.searchParams.get('turbineDeployedId');
-    console.log('Turbine: '+ turbineDeployedId);
+    console.log('Turbine: ' + turbineDeployedId);
     this.turbineDeployedId = turbineDeployedId;
 
     this.fetchSensorTimeSeries(turbineDeployedId);
