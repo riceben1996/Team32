ef27c533db973e817c0d5307e785abec8987914e
riceben1996 <tsisodia@iu.edu>
2018-10-28 00:06:02 -0400

    added highchart

--- public/js/kpiHours.app.js
+++ public/js/kpiHours.app.js
@@ -23,12 +23,71 @@ var kpiHoursApp = new Vue({
       this.sensorTimeSeries.forEach(
         (entry, index, arr) => {
           entry.heatRate = Number(entry.compressorEfficiency);
-          entry.output = Number(entry.output);
+          entry.firedHours = Number(entry.firedHours);
         }
       )
     },
 
-    /////////
+    buildHoursChart() {
+      Highcharts.chart('hoursChart', {
+          chart: {
+              zoomType: 'x'
+          },
+          title: {
+              text: 'Hours Over Time'
+          },
+          xAxis: {
+              type: 'datetime',
+              title: {
+                text: 'Date'
+              }
+          },
+          yAxis: {
+              title: {
+                  text: 'Hours'
+              }
+          },
+          legend: {
+              enabled: false
+          },
+          plotOptions: {
+              area: {
+                  fillColor: {
+                      linearGradient: {
+                          x1: 0,
+                          y1: 0,
+                          x2: 0,
+                          y2: 1
+                      },
+                      stops: [
+                          [0, Highcharts.getOptions().colors[0]],
+                          [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                      ]
+                  },
+                  marker: {
+                      radius: 2
+                  },
+                  lineWidth: 1,
+                  states: {
+                      hover: {
+                          lineWidth: 1
+                      }
+                  },
+                  threshold: null
+              }
+          },
+
+          series: [{
+              type: 'area',
+              name: 'Hours/Date',
+              data: kpiHoursApp.sensorTime.map( entry=>
+                [entry.dateCollected, entry.firedHours]
+              )
+          }]
+      });
+
+    },
+  },
 
   created() {
 
