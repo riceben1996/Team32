a786ea93dbc908f2426e6ca2c5098442642c52cc
riceben1996 <riceben@indiana.edu>
2018-10-31 11:43:01 -0400

    css

--- public/js/kpiAvailability.app.js
+++ public/js/kpiAvailability.app.js
@@ -1,73 +1,98 @@
 var kpiAvailabilityApp = new Vue({
-  el: '#kpiAvailability',
-  data: {
-    sensorTimeSeries: [],
-  },
+      el: '#kpiAvailability',
+      data: {
+        sensorTimeSeries: [],
+      },
 
-  methods: {
-    fetchSensorTimeSeries(turbineDeployedId) {
-      fetch('/api/sensorTimeSeries.php?turbineDeployedId=' + turbineDeployedId)
-        .then(response => response.json())
-        .then(json => {
-          kpiAvailabilityApp.sensorTimeSeries = json;
-          kpiAvailabilityApp.formatData();
-          kpiAvailabilityApp.buildAvailabilityChart();
+      methods: {
+        fetchSensorTimeSeries(turbineDeployedId) {
+          fetch('/api/sensorTimeSeries.php?turbineDeployedId=' + turbineDeployedId)
+            .then(response => response.json())
+            .then(json => {
+              kpiAvailabilityApp.sensorTimeSeries = json;
+              kpiAvailabilityApp.formatData();
+              kpiAvailabilityApp.buildAvailabilityChart();
+            })
+            .catch(err => {
+              console.log('Error getting data');
+              console.log(err);
+            })
+        },
+
+        formatData() {
+          this.sensorTimeSeries.forEach(
+            (entry, index, arr) => {
+              entry.dateCollected = Date.parse(entry.dataCollectedDate);
+              entry.availability = Number(entry.availability);
+            }
+          )
+        },
+
+        buildAvailabilityChart() {
+
+          var seriesOptions = [],
+            seriesCounter = 0,
+            names = ['Sensor1', 'Sensor2', 'Sensor3'];
+
+          $.each(names, function(i, name) {
+
+                $.getJSON('https://www.highcharts.com/samples/data/' + name.toLowerCase() + '-c.json', function(data) {
+
+                  seriesOptions[i] = {
+                    name: name,
+                    data: data
+                  };
+
+                  // As we're loading the data asynchronously, we don't know what order it will arrive. So
+                  // we keep a counter and create the chart when all the data is loaded.
+                  seriesCounter += 1;
+
+                  if (seriesCounter === names.length) {
+                    createChart();
+                  }
+                });
+              },
+
+
+                Highcharts.chart('availabilityChart', {
+                  xAxis: {
+                    enabled: true,
+                    type: 'datetime',
+                    title: {
+                      text: 'Date'
+                    }
+                  },
+                  yAxis: {
+                    enabled: true,
+                    title: {
+                      text: 'Availability Percentage'
+                    }
+                  },
+                  title: {
+                    text: 'Scatter Plot of Availability'
+                  },
+                  series: [{
+                    type: 'scatter',
+                    name: 'Observations',
+                    data: kpiAvailabilityApp.sensorTimeSeries.map(entry => [entry.dateCollected, entry.availability]),
+                    marker: {
+                      radius: 4
+                    }
+                  }]
+                });
+              },
+            },
+
+            created() {
+
+              const url = new URL(window.location.href);
+              const turbineDeployedId = url.searchParams.get('turbineDeployedId');
+              //  console.log('Turbine: ' + turbineDeployedId);
+              this.turbineDeployedId = turbineDeployedId;
+
+              this.fetchSensorTimeSeries(turbineDeployedId);
+
+
+
+            }
         })
-        .catch(err => {
-          console.log('Error getting data');
-          console.log(err);
-        })
-    },
-
-    formatData() {
-      this.sensorTimeSeries.forEach(
-        (entry, index, arr) => {
-          entry.dateCollected = Date.parse(entry.dataCollectedDate);
-          entry.availability = Number(entry.availability);
-        }
-      )
-    },
-
-    buildAvailabilityChart() {
-      Highcharts.chart('availabilityChart', {
-        xAxis: {
-          enabled: true,
-          type: 'datetime',
-          title: {
-            text: 'Date'
-          }
-        },
-        yAxis: {
-          enabled: true,
-          title: {
-            text: 'Availability Percentage'
-          }
-        },
-        title: {
-          text: 'Scatter Plot of Availability'
-        },
-        series: [{
-          type: 'scatter',
-          name: 'Observations',
-          data: kpiAvailabilityApp.sensorTimeSeries.map(entry => [entry.dateCollected, entry.availability]),
-          marker: {
-            radius: 4
-          }
-        }]
-      });
-    },
-  },
-
-  created() {
-
-    const url = new URL(window.location.href);
-    const turbineDeployedId = url.searchParams.get('turbineDeployedId');
-  //  console.log('Turbine: ' + turbineDeployedId);
-    this.turbineDeployedId = turbineDeployedId;
-
-    this.fetchSensorTimeSeries(turbineDeployedId);
-
-
-
-  }
-})
