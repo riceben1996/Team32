3a5e9d8a731344b6371294a812c75278846dd467
riceben1996 <riceben@indiana.edu>
2018-11-02 09:48:06 -0400

    testing

--- public/js/kpiAvailability.app.js
+++ public/js/kpiAvailability.app.js
@@ -34,8 +34,8 @@ var kpiAvailabilityApp = new Vue({
       var series = {};
       console.log(series);
 
-      Array.prototype.forEach.call(series, function(i) {
-      // series.foreach(function(i) {
+      // Array.prototype.forEach.call(series, function(i) {
+      this.sensorTimeSeries.foreach(function(i) {
 
         console.log("FOR EACH BEFORE IF");
 
