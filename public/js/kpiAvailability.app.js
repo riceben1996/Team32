a8c6db9af7195e57149bc2ae89725511ccdcd53d
riceben1996 <riceben@indiana.edu>
2018-11-02 09:27:42 -0400

    asdf

--- public/js/kpiAvailability.app.js
+++ public/js/kpiAvailability.app.js
@@ -66,60 +66,60 @@ var kpiAvailabilityApp = new Vue({
 
 
 
-      buildAvailabilityChart() {
+    buildAvailabilityChart() {
 
-         Highcharts.chart('availabilityChart', {
-             chart: {
-                 zoomType: 'x'
-             },
-             title: {
-                 text: 'Availability Over Time'
-             },
-             xAxis: {
-                 type: 'datetime',
-                 title: {
-                     text: 'Date'
-                 }
-             },
-             yAxis: {
-                 title: {
-                     text: 'Availability Percentage Over Time'
-                 }
-             },
-             legend: {
-                 enabled: false
-             },
-             plotOptions: {
-                 area: {
-                     fillColor: {
-                         linearGradient: {
-                             x1: 0,
-                             y1: 0,
-                             x2: 0,
-                             y2: 1
-                         },
-                         stops: [
-                             [0, Highcharts.getOptions().colors[0]],
-                             [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
-                         ]
-                     },
-                     marker: {
-                         radius: 2
-                     },
-                     lineWidth: 1,
-                     states: {
-                         hover: {
-                             lineWidth: 1
-                         }
-                     },
-                     threshold: null
-                 }
-             },
+      Highcharts.chart('availabilityChart', {
+        chart: {
+          zoomType: 'x'
+        },
+        title: {
+          text: 'Availability Over Time'
+        },
+        xAxis: {
+          type: 'datetime',
+          title: {
+            text: 'Date'
+          }
+        },
+        yAxis: {
+          title: {
+            text: 'Availability Percentage Over Time'
+          }
+        },
+        legend: {
+          enabled: false
+        },
+        plotOptions: {
+          area: {
+            fillColor: {
+              linearGradient: {
+                x1: 0,
+                y1: 0,
+                x2: 0,
+                y2: 1
+              },
+              stops: [
+                [0, Highcharts.getOptions().colors[0]],
+                [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+              ]
+            },
+            marker: {
+              radius: 2
+            },
+            lineWidth: 1,
+            states: {
+              hover: {
+                lineWidth: 1
+              }
+            },
+            threshold: null
+          }
+        },
 
-             series: Object.values(series)
-         });
-       },
+        series: Object.values(series)
+      });
     },
+  },
 
   created() {
 
